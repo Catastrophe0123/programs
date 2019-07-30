65604df12fda72cb2bcf734e2d2680a1cbbf8813
Catastrophe0123 <43993832+Catastrophe0123@users.noreply.github.com>
2019-07-30 23:00:29 +0530

    Update TODOListPySimpleGUI-clean.py

--- TODOListPySimpleGUI-clean.py
+++ TODOListPySimpleGUI-clean.py
@@ -1,56 +1,62 @@
 import PySimpleGUI as s
+import fileModule
+
 
 lis = []
-
-def readFile(lis):
-    with open("F:\\Navaneeth\College stuff\\asssignments and projects\programming\PySimpleGUI\\file.txt","r") \
-            as f:
-        if(lis == []):
-            return lis
-        lis = f.read().splitlines()
-    return lis
-
-def writeToFile():
-    with open("F:\\Navaneeth\College stuff\\asssignments and projects\programming\PySimpleGUI\\file.txt","w") \
-            as f:
-        for i in range(len(lis)):
-            f.write(lis[i])
-            f.write("\n")
-
-lis = readFile(lis)
+completed = []
+lis = fileModule.readFile()
+completed = fileModule.readCompleted()
 layout = [[s.Text("TODO LIST ")],[s.Text("New Entry : "),s.InputText("",key = "entry")],
-          [s.Listbox(values=lis,key = "list",size=(60,6), enable_events=True)],
-          [s.CalendarButton("Choose Date", target="dateDisp", key='date'),s.InputText("",key = "dateDisp",do_not_clear=False)],
-          [s.Button("add"),s.Button("delete"),s.Button("prioritize"),s.Exit()],
-          [s.Text("",auto_size_text=False,key = "tell")]]
+          [s.Listbox(values=lis,key = "list",size=(40,6), enable_events=True), s.Listbox(values=completed,key = "complete",size=(40,6), enable_events=True)],
+          [s.CalendarButton("Choose Date", target="dateDisp", key='date'),s.InputText("",key = "dateDisp", disabled=True ,do_not_clear=False)],
+          [s.Slider(range=(5,1,-1),default_value=5,orientation="horizontal",key="priority")],
+          [s.Button("add"),s.Button("delete"),s.Button("prioritize"),s.Button("completed"),s.Exit()],
+          [s.Text("", auto_size_text=False, key="tell")]]
 
-window = s.Window("my first SGUI ",layout)
+window = s.Window("my first GUI ", layout)
 
 while True:
     event, entries = window.Read()
-
+    print(event, entries)
     if event is None or event == "Exit":
         break
 
     elif (event == "add"):
-        x = entries["entry"]+" "+entries["dateDisp"]
+        if(entries["dateDisp"] == ""):
+            window.Element("tell").Update("Please input a date")
+            continue
+
+        x = entries["entry"]+" "+entries["dateDisp"]+" "+str(int(entries["priority"]))
         lis.append(x)
         window.FindElement("list").Update(lis)
         window.Element("tell").Update("item added")
-        writeToFile()
+        fileModule.writeToFile(lis) #working
 
     elif( event == "delete"):
         lis.remove(''.join(entries["list"]))
         window.FindElement("list").Update(lis)
         window.Element("tell").Update("item deleted")
-        writeToFile()
+        fileModule.writeToFile(lis) #working
 
     elif( event == "prioritize"):
-        x = ""
-        x = ''.join(entries["list"])
-        lis.insert(0, lis.pop(lis.index(x)))
+        for i in range(len(lis)):
+            min = i
+            for j in range(i+1, len(lis)):
+                if(lis[min][-1] > lis[j][-1]):
+                    min = j
+            lis[i],lis[min] = lis[min],lis[i]
         window.FindElement("list").Update(lis)
-        window.Element("tell").Update("item prioritized")
-        writeToFile()
+        window.Element("tell").Update("prioritized")
+        fileModule.writeToFile(lis) #working
+
+    elif( event == "completed"):
+        lis.remove(''.join(entries["list"]))
+        completed.append(''.join(entries["list"]))
+        window.FindElement("list").Update(lis)
+        window.FindElement("complete").Update(completed)
+        window.Element("tell").Update("item completed")
+        fileModule.writeToCompleted(completed) #working
+
+
 
 window.Close()
